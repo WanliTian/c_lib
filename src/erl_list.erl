8574197e7172fbf352b3e60aabd014f85312b426
WanliTian <buaatianwanli@gmail.com>
2016-01-05 10:44:10 +0800

    erl_list:index eunit

--- src/erl_list.erl
+++ src/erl_list.erl
@@ -4,6 +4,20 @@
     index/2
 ]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -spec index(list(), term()) -> integer().
 index(List, Term) ->
     string:str(List, [Term]).
+
+-ifdef(TEST).
+
+index_test() ->
+    0 = ?MODULE:index(["one", "two", "three"], "zero"),
+    1 = ?MODULE:index(["one", "two", "three"], "one"),
+    2 = ?MODULE:index(["one", "two", "three"], "two"),
+    3 = ?MODULE:index(["one", "two", "three"], "three").
+
+-endif.
