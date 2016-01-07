06f1c51500a9ab974eceb96c10e5a0cc65d30354
WanliTian <buaatianwanli@gmail.com>
2016-01-07 20:17:32 +0800

    add timestamp

--- src/erl_timer.erl
+++ src/erl_timer.erl
@@ -1,1 +1,13 @@
 -module(erl_timer).
+
+-export([
+    unixtime/0,
+    timestamp/0
+]).
+
+unixtime() ->
+    {M, S, _} = os:timestamp(),
+    M * 1000000 + S.
+
+timestamp() ->
+    ?MODULE:unixtime().
